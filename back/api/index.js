66fad55fecc6099ccd9671c91ed1fcdecd856725
dguerrem <99349002+dguerrem@users.noreply.github.com>
2024-09-12 12:07:59 +0200

    (D) feat: Sin Export

--- back/api/index.js
+++ back/api/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('¡Hola, mundo!');
 });
 
-module.exports = app;
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
