ee7c0aeea70d3facf5e785e3bffbc77a64f36d6d
dguerrem <99349002+dguerrem@users.noreply.github.com>
2024-09-18 22:00:02 +0200

    (D) faat: Cors Policy Backend

--- back/index.js
+++ back/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 3000;
 const rolesRoutes = require('./routes/rolesRoutes');
 const usersRoutes = require('./routes/usersRoutes');
 
 app.use(express.json());
+app.use(cors({
+  origin: 'https://66eb2c5191be00865272a46c--fancy-crisp-0e0c22.netlify.app'
+}));
 
 app.get('/', (req, res) => {
   res.send('API GymTracker');
